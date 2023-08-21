2ab5985dee3ca776b09ed1ff5479e465e904ded5
Thabang Kganana <thabangx12@gmail.com>
2023-08-21 08:33:30 +0200

    first

--- api/controller/index.js
+++ api/controller/index.js
@@ -12,9 +12,6 @@ routes.get('/users', (req, res) => {
 routes.get('/user/:id', (req, res) => {
     users.fetchUser(req, res)
 })
-routes.post('/register', bodyParser.json(), (req, res) => {
-    users.register(req, res)
-})
 routes.put('/user/:id', bodyParser.json(), (req, res) => {
     users.updateUser(req, res)
 })
@@ -24,10 +21,6 @@ routes.patch('/user/:id', bodyParser.json(), (req, res) => {
 routes.delete('/user/:id', (req, res) => {
     users.deleteUser(req, res)
 })
-routes.post('/login', bodyParser.json(), (req, res)=>{
-    users.login(req, res)
-})
-// books router
 routes.get('/books', verifyAToken, (req, res) =>{
     books.fetchBooks(req, res)
 })
