d5bd9eb94671ea511b71761acc060e4a61dc3251
Thabang Kganana <thabangx12@gmail.com>
2023-08-23 22:50:36 +0200

    you will have to pull your socks up, did your frontEnd aswell , just optimise better will provide better product information tomorrow at college

--- user_gui/src/components/NavBar.vue
+++ user_gui/src/components/NavBar.vue
@@ -12,7 +12,7 @@
                     <a class="nav-link active" aria-current="page" href="/">Home</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Clothing & Accesories</a>
+                    <a class="nav-link" href="/product">Clothing & Accesories</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="/admin">Admin</a>
--- user_gui/src/views/ProductView.vue
+++ user_gui/src/views/ProductView.vue
@@ -1,15 +1,52 @@
 <template>
-    <div>
-
+    <div class="container">
+      <nav-bar />
+      <div class="row">
+        <h2 class="display-2">Products</h2>
+      </div>
+      <div class="row justify-content-center gap-3">
+        <!-- Display a list of products -->
+        <div v-for="product in products" class="card" style="width: 18rem;" :key="product.productId">
+          <img :src="product.image" class="card-img-top" :alt="product.name">
+          <div class="card-body">
+            <h5 class="card-title">{{ product.name }}</h5>
+            <p class="card-text">
+              <span>Price: ${{ product.price }}</span>
+            </p>
+            <!-- Add buttons for editing and deleting products -->
+            <button @click="editProduct(product)" class="btn btn-primary">EDIT</button>
+            <button @click="deleteProduct(product.productId)" class="btn btn-danger">DELETE</button>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
+  
+  <script>
+  import NavBar from '@/components/NavBar.vue';
+  
+  export default {
+    components: { NavBar },
+    computed: {
+      products() {
+        return this.$store.state.Products;
+      },
+    },
+    mounted() {
+      this.$store.dispatch('fetchProducts');
+    },
+    methods: {
+      editProduct(product) {
 
-<script>
-    export default {
-        
-    }
-</script>
+    },
+      deleteProduct(productId) {
 
-<style scoped>
+    },
+    },
+  };
+  </script>
+  
+  <style scoped>
 
-</style>
+  </style>
+  
